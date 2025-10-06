a64dc0b2baf0cb3d32e76b342f93b5ae83090334
Luiz henrique santana <luizhenriquesantana818.com>
2025-10-06 14:43:49 -0300

    header

--- vite-project/src/components/Header/index.jsx
+++ vite-project/src/components/Header/index.jsx
@@ -1,26 +1,42 @@
-import './Header.css';
-import { Link } from 'react-router-dom';
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return(
-        <header className='header'>
-            {/* Logo */}
-            <div className="logo">
-                <Link to='/'>
-                <span className='logo-1'>L</span>
-                <span className="logo-2">H</span>
-                <span className="logo-3">S</span>
-                </Link>
-            </div>
+  return (
+    <header className="header">
+      {/* Logo */}
+      <div className="logo">
+        <Link to="/">
+          <span className="logo-1">L</span>
+          <span className="logo-2">H</span>
+          <span className="logo-3">S</span>
+        </Link>
+      </div>
 
-            <div className="search-bar">
-                <input type="search" placeholder='Buscar'/>
-                <div className='location'>
-                    <button className='search-btn'>
-                    <ion-icon name="search-outline"></ion-icon>
-                    </button>
-                </div>
-            </div>
-        </header>
-    )
+      <div className="search-bar">
+        <input type="search" placeholder="Buscar" />
+        <div className="location">
+          <button className="search-btn">
+            <ion-icon name="search-outline"></ion-icon>
+          </button>
+        </div>
+      </div>
+
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/" className="entrarBtn">
+              Sobre
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/" className="anunciarBtn">
+              Anunciar
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
